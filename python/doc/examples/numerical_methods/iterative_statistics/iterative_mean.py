e4b1bbd6cef105cb3e940c96ec387f50224f7e59
Kieran Delamotte <delamotte@imacs.polytechnique.fr>
2021-06-14 11:40:49 +0200

    Add iterative statistics examples

---
+++ python/doc/examples/numerical_methods/iterative_statistics/iterative_mean.py
@@ -0,0 +1,51 @@
+"""
+Iterative mean
+==============
+"""
+
+# %%
+# In this example, we show how to use the `IterativeMean` class. This algorithm computes an estimate of the mean using iterative algorithms. In other words, this algorithm can be used to iteratively update the estimate of the mean using one point at a time instead of using the full sample. 
+
+# %%
+import openturns as ot
+
+# %%
+# Create the mean object
+
+# %%
+dim = 5
+myMean = ot.IterativeMean(dim)
+
+# %%
+# Perform the simulations. In the following session, we increment using the `increment` method one `Point` at a time.
+
+# %%
+n = ot.Normal(dim)
+size = 50
+for i in range(size):
+    point = n.getRealization()
+    myMean.increment(point)
+
+# %%
+# The `getMean` method returns the sample mean.
+
+# %%
+myMean.getMean()
+
+# %%
+# The `getIteration` method return the sample size.
+
+# %%
+myMean.getIteration()
+
+# %%
+# We can also increment with a `Sample`.
+
+# %%
+sample = n.getSample(size)
+myMean.increment(sample)
+
+# %%
+print ("Iteration " + str(myMean.getIteration()))
+print (myMean.getMean())
+
